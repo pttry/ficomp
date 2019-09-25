729b5f34ff3b1fc060db964f0a852172c271a934
Janne Huovari <janne.huovari@ptt.fi>
2019-09-25 10:13:08 +0300

    name change in bis

--- data-raw/get_BIS.R
+++ data-raw/get_BIS.R
@@ -20,13 +20,13 @@ names(wbis_broad_sheets) <- wbis_broad_sheets
 
 weights_bis_narrow <-
   map_dfr(wbis_narrow_sheets, ~readxl::read_xlsx(wbis_narrow_temp, sheet = ., range = "B6:AC33"), .id = "time") %>%
-  rename(geo = ...1) %>%
-  gather(geo2, weight, -geo, - time)
+  rename(geo_base = ...1) %>%
+  gather(geo, weight, -geo_base, - time)
 
 weights_bis_broad <-
   map_dfr(wbis_broad_sheets, ~readxl::read_xlsx(wbis_broad_temp, sheet = ., range = "B6:BJ66"), .id = "time") %>%
-  rename(geo = ...1) %>%
-  gather(geo2, weight, -geo, - time)
+  rename(geo_base = ...1) %>%
+  gather(geo, weight, -geo_base, - time)
 
 
 usethis::use_data(weights_bis_narrow, overwrite = TRUE)
