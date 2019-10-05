c4f230ad52ac21809c7b0a7f125aaf73d21e7cfc
Janne Huovari <janne.huovari@ptt.fi>
2019-10-05 20:45:07 +0300

    oecd ulc

--- data-raw/get_OECD.R
+++ data-raw/get_OECD.R
@@ -5,9 +5,11 @@ library(dplyr)
 library(forcats)
 library(lubridate)
 
-# dataset_list <- get_datasets()
+dataset_list <- get_datasets()
 # search_dataset("Quarterly National Accounts", data = dataset_list)
+search_dataset("weights", data = dataset_list)
 dataset_id <- "QNA"
+
 
 stan_str <- get_data_structure(dataset_id)
 
@@ -28,7 +30,7 @@ measure_list <- stan_str$MEASURE %>%
   filter(
     label %in% c(
       "National currency, current prices, quarterly levels, seasonally adjusted",
-      "National currency, chained volume estimates, national reference year, quarterly levels, seasonally adjusted",
+      "Volume index, OECD reference year, seasonally adjusted",
       "Hours worked, seasonally adjusted",
       "Jobs, seasonally adjusted"
     )
@@ -40,6 +42,44 @@ geo <- c("FIN", "USA")
 oecd_dat_Q_0 <- get_dataset(dataset = dataset_id,
                           filter = list(geo, subject_list, measure_list, "Q"))
 
-oecd_dat_Q <- oecd_dat_Q_0 %>%
-  mutate_if(is.character, as_fa)
+oecd_dat_Q_test <- get_dataset(dataset = dataset_id,
+                            filter = list("USA"), start_time = "2015", end_time = "2015")
 
+test <- oecd_dat_Q_test %>%
+  filter(obsTime == "2015-Q1")
+
+stan_str$SUBJECT %>%
+  left_join(select(test, id = SUBJECT, obsValue)) %>% View()
+
+oecd_dat_Q <- oecd_dat_Q_0 %>%
+  mutate_if(is.character, as_factor) %>%
+  filter(obsTime == "2015-Q1")
+
+table(is.na(oecd_dat_Q$obsValue))
+
+# Main economic indicators
+mei_str <- get_data_structure("MEI")
+# View(mei_str$SUBJECT)
+
+# ULC
+
+ulc_str <- get_data_structure("ULC_EEQ")
+
+ulc_oecd_dat0 <-
+  get_dataset("ULC_EEQ",
+               filter = list(
+                 ulc_str$LOCATION$id,
+                 ulc_str$SUBJECT$id,
+                 "IXOBSA",             # Index, seasonally adjusted
+                 "Q"))
+
+ulc_oecd_dat <- ulc_oecd_dat0 %>%
+  mutate_if(is.character, as_factor) %>%
+  mutate(time = yq(obsTime))
+
+
+ulc_oecd_dat %>%
+  filter(LOCATION %in% c("FIN", "USA", "SWE"),
+         SUBJECT == "ULQEUL01") %>%
+  ggplot(aes(time, obsValue, colour = LOCATION)) +
+  geom_line()
