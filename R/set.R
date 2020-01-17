e24515e09c1d91eeb28bb2b1560d5dae289a8eb1
Janne Huovari <janne.huovari@ptt.fi>
2020-01-17 16:44:27 +0200

    Vignette report

--- R/set.R
+++ R/set.R
@@ -2,10 +2,10 @@
 #'
 #'
 
-set_proj_theme <- function(){
-  ggptt::set_ptt(14, "sans")
+set_proj_theme <- function(base_size = 14){
+  ggptt::set_ptt(base_size, "sans")
   ggplot2::theme_update(
-    legend.text = ggplot2::element_text(size = 14),
+    legend.text = ggplot2::element_text(size = base_size),
     plot.subtitle = ggplot2::element_text(colour = "grey40"),
-    plot.caption = ggplot2::element_text(size = 10, face = "plain", colour = "grey40"))
+    plot.caption = ggplot2::element_text(size = base_size-4, face = "plain", colour = "grey40"))
 }
