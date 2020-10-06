5e249ef5ffa3a9b9d2014ccb5a478b816ccfd9c0
Janne Huovari <janne.huovari@ptt.fi>
2020-10-06 08:57:58 +0300

    ggthemes

--- R/set.R
+++ R/set.R
@@ -14,7 +14,7 @@ set_proj_theme <- function(base_size = 12){
 
 
 set_board_theme <- function(base_size = 11){
-  ggptt::set_gg(theme_ptt(base_size, "sans"), tableau_color_pal(palette = "Color Blind"))
+  ggptt::set_gg(theme_ptt(base_size, "sans"), ggthemes::tableau_color_pal(palette = "Color Blind"))
   ggplot2::theme_update(
     legend.text = ggplot2::element_text(size = base_size),
     axis.title = ggplot2::element_text(colour = "grey20", size = base_size - 2),
