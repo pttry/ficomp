45e00cc3c1a4d185e80568e022151d82417a558a
Janne Huovari <janne.huovari@ptt.fi>
2020-10-09 16:48:55 +0300

    Board plots

--- R/plotting.R
+++ R/plotting.R
@@ -56,3 +56,26 @@ ggsave_twitter <- function(filename, plot = last_plot()){
                   width = 16/2,
                   height = 9/2)
 }
+
+
+#' Save figures for competitiveness board report
+#'
+#' to Downloads folder
+#'
+#' Uses ggsave.
+#'
+#' @param filename File name to create on disk.
+#' @param plot Plot to save, defaults to last plot displayed.
+#' @param width Plot size
+#' @param height Plot size
+#'
+#' @export
+
+save_board_figs <- function(filename,
+                      plot = last_plot(),
+                      width = 13.5,
+                      height = 13.5){
+  plot <- plot
+  ggplot2::ggsave(file.path(Sys.getenv("USERPROFILE"),"Downloads", paste0(filename, ".pdf")), width = width, height = height, units = "cm")
+  ggplot2::ggsave(file.path(Sys.getenv("USERPROFILE"),"Downloads", paste0(filename, ".png")), width = width, height = height, units = "cm")
+}
